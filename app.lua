d7a38e0ac3c73962e154c18d4ed0e29023dd6712
Rubens da Costa <rubens.costa.tkd@gmail.com>
2026-04-15 19:31:15 -0300

    feat: refactor flag handling and remove online event capture
    - Remove `BetterFlags.capturaDeEventoOnline()` call from start function
    - Simplify and clean up flag handler logic with consistent formatting
    - Refactor slow car event handling to use direct function call
    - Improve readability of condition checks and boolean assignments
    - Update slow car event structure and handling for clarity
    - Remove unused local variable `eventoCarroLento`

--- app.lua
+++ app.lua
@@ -58,7 +58,6 @@ BetterFlags.start()
 function BetterFlags.start()
     ac.blockSystemMessages("$CSP0:")
     BetterFlags.mensagemDeBoasVindas()
-    BetterFlags.capturaDeEventoOnline()
     BetterFlags.makeFlags()
 end
 
@@ -83,7 +82,6 @@ function BetterFlags.mensagemDeBoasVindas()
 end
 
 function BetterFlags.flagHandler()
-
     if ((trackProgress > noOvertake1_S) and (trackProgress < noOvertake1_E)) or ((trackProgress > noOvertake2_S) and (trackProgress < noOvertake2_E) or ((trackProgress > noOvertake3_S) and (trackProgress < noOvertake3_E))) or settingsOverride then
         currentFlags[1][1] = true
     else
@@ -91,11 +89,9 @@ function BetterFlags.flagHandler()
     end
 
     if BetterFlags.shouldSlowCar() or settingsOverride then
-
-    currentFlags[2][1] = true
-
+        currentFlags[2][1] = true
     else
-        currentFlags[2][1] =  false
+        currentFlags[2][1] = false
     end
 
     if BetterFlags.shouldMeatball() or settingsOverride then
@@ -103,36 +99,33 @@ function BetterFlags.flagHandler()
     else
         currentFlags[3][1] = false
     end
-
-
 end
 
 function BetterFlags.shouldSlowCar()
-    if (CAR.speedKmh < 30) and not(CAR.isInPitlane) and (CAR.wheelsOutside < 3) and (SIM.timeToSessionStart < -10000) then
+    if (CAR.speedKmh < 30) and not (CAR.isInPitlane) and (CAR.wheelsOutside < 3) and (SIM.timeToSessionStart < -10000) then
         if lastSlowCarBroadcastAttempt + slowCarCooldown < BetterFlags.totalElapsedTime then
             lastSlowCarBroadcastAttempt = BetterFlags.totalElapsedTime
             --ac.broadcastSharedEvent("broadcastSlowCar", trackProgress)
-            BetterFlags.slowCarEvent({slowCarProgress=trackProgress})
+            slowCarEvent({ slowCarProgress = trackProgress })
         end
 
         return true
-    elseif  lastSlowCarRecieve + slowCarFlagPersist > BetterFlags.totalElapsedTime then
+    elseif lastSlowCarRecieve + slowCarFlagPersist > BetterFlags.totalElapsedTime then
         return true
     else
         return false
     end
-
 end
 
 function BetterFlags.shouldMeatball()
     if (CAR.wheels[0].suspensionDamage > meatballThreshold) or
-    (CAR.wheels[1].suspensionDamage > meatballThreshold) or
-    (CAR.wheels[2].suspensionDamage > meatballThreshold) or
-    (CAR.wheels[3].suspensionDamage > meatballThreshold) or
-    CAR.wheels[0].isBlown or
-    CAR.wheels[1].isBlown or
-    CAR.wheels[2].isBlown or
-    CAR.wheels[3].isBlown
+        (CAR.wheels[1].suspensionDamage > meatballThreshold) or
+        (CAR.wheels[2].suspensionDamage > meatballThreshold) or
+        (CAR.wheels[3].suspensionDamage > meatballThreshold) or
+        CAR.wheels[0].isBlown or
+        CAR.wheels[1].isBlown or
+        CAR.wheels[2].isBlown or
+        CAR.wheels[3].isBlown
     --CAR.wheels[4].isBlown
 
     then
@@ -142,118 +135,110 @@ function BetterFlags.shouldMeatball()
     end
 end
 
-local eventoCarroLento
-function BetterFlags.capturaDeEventoOnline()
-    eventoCarroLento = ac.OnlineEvent({
-        structCarroLento = ac.StructItem.key("carroLento"),
-        structLocalCarroLento = ac.StructItem.float(),
-    }, function(sender, data)
-        ac.debug('Got message: from', sender and sender.index or -1)
-        ac.debug('Got message: text', data.slowCarProgress)
-        if ((data.slowCarProgress + 0.01) > BetterFlags.trackProgress - math.floor((BetterFlags.trackProgress + BetterFlags.slowCarDistance)) and (data.slowCarProgress < ((BetterFlags.trackProgress + BetterFlags.slowCarDistance) - math.floor((BetterFlags.trackProgress + BetterFlags.slowCarDistance))))) then
-            BetterFlags.lastSlowCarRecieve = BetterFlags.totalElapsedTime
-        end
-    end, ac.SharedNamespace.ServerScript)
+slowCarEvent = ac.OnlineEvent({
+    -- message structure layout:
+    key = ac.StructItem.key('slowCarEvent'),
+    slowCarProgress = ac.StructItem.float(),
+}, function(sender, data)
+    ac.debug('Got message: from', sender and sender.index or -1)
+    ac.debug('Got message: text', data.slowCarProgress)
+    if ((data.slowCarProgress + 0.01) > trackProgress - math.floor((trackProgress + slowCarDistance)) and (data.slowCarProgress < ((trackProgress + slowCarDistance) - math.floor((trackProgress + slowCarDistance))))) then
+        lastSlowCarRecieve = totalElapsedTime
+    end
+end, ac.SharedNamespace.ServerScript)
     ac.debug("!version", "betterflags v0.51")
 
-end
 
 function BetterFlags.makeFlags()
-
-    local startFlag = ui.ExtraCanvas(vec2(256,256)) 
+    local startFlag = ui.ExtraCanvas(vec2(256, 256))
     startFlag:setName("startFlag")
-    startFlag:update(function (dt)
+    startFlag:update(function(dt)
         ui.drawRaceFlag(ac.FlagType.Start)
     end)
 
-    local cautionFlag = ui.ExtraCanvas(vec2(256,256)) 
+    local cautionFlag = ui.ExtraCanvas(vec2(256, 256))
     cautionFlag:setName("cautionFlag")
-    cautionFlag:update(function (dt)
+    cautionFlag:update(function(dt)
         ui.drawRaceFlag(ac.FlagType.Caution)
     end)
 
-    local slipperyFlag = ui.ExtraCanvas(vec2(256,256)) 
+    local slipperyFlag = ui.ExtraCanvas(vec2(256, 256))
     slipperyFlag:setName("slipperyFlag")
-    slipperyFlag:update(function (dt)
+    slipperyFlag:update(function(dt)
         ui.drawRaceFlag(ac.FlagType.Slippery)
     end)
 
-    local blackFlag = ui.ExtraCanvas(vec2(256,256)) 
+    local blackFlag = ui.ExtraCanvas(vec2(256, 256))
     blackFlag:setName("blackFlag")
-    blackFlag:update(function (dt)
+    blackFlag:update(function(dt)
         ui.drawRaceFlag(ac.FlagType.Stop)
     end)
 
-    local whiteFlag = ui.ExtraCanvas(vec2(256,256)) 
+    local whiteFlag = ui.ExtraCanvas(vec2(256, 256))
     whiteFlag:setName("whiteFlag")
-    whiteFlag:update(function (dt)
+    whiteFlag:update(function(dt)
         ui.drawRaceFlag(ac.FlagType.SlowVehicle)
     end)
 
-    local ambulanceFlag = ui.ExtraCanvas(vec2(256,256)) 
+    local ambulanceFlag = ui.ExtraCanvas(vec2(256, 256))
     ambulanceFlag:setName("ambulanceFlag")
-    ambulanceFlag:update(function (dt)
+    ambulanceFlag:update(function(dt)
         ui.drawRaceFlag(ac.FlagType.Ambulance)
     end)
 
-    local blackWhiteFlag = ui.ExtraCanvas(vec2(256,256)) 
+    local blackWhiteFlag = ui.ExtraCanvas(vec2(256, 256))
     blackWhiteFlag:setName("blackWhiteFlag")
-    blackWhiteFlag:update(function (dt)
+    blackWhiteFlag:update(function(dt)
         ui.drawRaceFlag(ac.FlagType.ReturnToPits)
     end)
 
-    local meatballFlag = ui.ExtraCanvas(vec2(256,256)) 
+    local meatballFlag = ui.ExtraCanvas(vec2(256, 256))
     meatballFlag:setName("meatballFlag")
-    meatballFlag:update(function (dt)
+    meatballFlag:update(function(dt)
         ui.drawRaceFlag(ac.FlagType.MechanicalFailure)
     end)
 
-    local blueFlag = ui.ExtraCanvas(vec2(256,256)) 
+    local blueFlag = ui.ExtraCanvas(vec2(256, 256))
     blueFlag:setName("blueFlag")
-    blueFlag:update(function (dt)
+    blueFlag:update(function(dt)
         ui.drawRaceFlag(ac.FlagType.FasterCar)
     end)
 
-    local code60Flag = ui.ExtraCanvas(vec2(256,256)) 
+    local code60Flag = ui.ExtraCanvas(vec2(256, 256))
     code60Flag:setName("code60Flag")
-    code60Flag:update(function (dt)
+    code60Flag:update(function(dt)
         ui.drawRaceFlag(ac.FlagType.Code60)
     end)
 
-    flagsWindow = ui.ExtraCanvas(vec2(windowWidth,windowHeight))
+    flagsWindow = ui.ExtraCanvas(vec2(windowWidth, windowHeight))
     flagsWindow:setName("FlagWindow")
 
-    NoOver = {true,slipperyFlag}
-    Slow = {true, whiteFlag}
-    Meatball = {true, meatballFlag}
-    Code60 = {false , code60Flag}
+    NoOver = { true, slipperyFlag }
+    Slow = { true, whiteFlag }
+    Meatball = { true, meatballFlag }
+    Code60 = { false, code60Flag }
 
-    currentFlags = {NoOver,Slow,Meatball,Code60}
-
-
+    currentFlags = { NoOver, Slow, Meatball, Code60 }
 end
 
-
 function script.update(dt)
-
     BetterFlags.totalElapsedTime = SIM.currentSessionTime
     BetterFlags.trackProgress = CAR.splinePosition
 
-        ac.debug('Elapsed totalElapsedTime', BetterFlags.totalElapsedTime)
-        --ac.debug('asconfig', parsedConfig)
---        ac.debug('Progress', ac.flagType.Ambulance)
-        --ac.debug('speed', CAR.wheels[1].suspensionDamage)
-        --ac.debug('whatever', configChecks)
-        --ac.debug('uiScale', uiScale)
-        --ac.debug('mirrorscale', mirrorScale)    
-        --ac.debug('windowHeight', image1posy) 
+    ac.debug('Elapsed totalElapsedTime', BetterFlags.totalElapsedTime)
+    --ac.debug('asconfig', parsedConfig)
+    --        ac.debug('Progress', ac.flagType.Ambulance)
+    --ac.debug('speed', CAR.wheels[1].suspensionDamage)
+    --ac.debug('whatever', configChecks)
+    --ac.debug('uiScale', uiScale)
+    --ac.debug('mirrorscale', mirrorScale)
+    --ac.debug('windowHeight', image1posy)
 
-        ac.debug("batt", currentFlags)
-        ac.debug("dm", SIM.directMessagingAvailable)
-        ac.debug("udp", SIM.directUDPMessagingAvailable)
+    ac.debug("batt", currentFlags)
+    ac.debug("dm", SIM.directMessagingAvailable)
+    ac.debug("udp", SIM.directUDPMessagingAvailable)
 
     BetterFlags.flagHandler()
-    
 end
 
 --###############################################################################
@@ -267,17 +252,16 @@ end)
 
 --UI FUNCTIONSSS
 ac.onResolutionChange(function()
-    windowWidth, windowHeight = ac.getSim().windowWidth,ac.getSim().windowHeight
+    windowWidth, windowHeight = ac.getSim().windowWidth, ac.getSim().windowHeight
 
-        local mirrorScale = windowHeight/1800
-        
+    local mirrorScale = windowHeight / 1800
 
-        local vmirrorTop = (85/uiScale)
-        local vmirrorLeft = ((windowWidth/2)-(425.45525*mirrorScale)-2)/uiScale
-        local vmirrorBottom = ((213.78521*mirrorScale+83.3)/uiScale)
-        local vmirrorRight = ((windowWidth/2)+(425.45525*mirrorScale)+2)/uiScale
-    flagsWindow = ui.ExtraCanvas(vec2(windowWidth,windowHeight))
-        
+
+    local vmirrorTop = (85 / uiScale)
+    local vmirrorLeft = ((windowWidth / 2) - (425.45525 * mirrorScale) - 2) / uiScale
+    local vmirrorBottom = ((213.78521 * mirrorScale + 83.3) / uiScale)
+    local vmirrorRight = ((windowWidth / 2) + (425.45525 * mirrorScale) + 2) / uiScale
+    flagsWindow = ui.ExtraCanvas(vec2(windowWidth, windowHeight))
 end)
 
 ui.registerOnlineExtra(ui.Icons.Flag, "BetterFlags Settings", function() return true end,
@@ -285,49 +269,43 @@ ui.registerOnlineExtra(ui.Icons.Flag, "BetterFlags Settings", function() return 
     function() --UiCallback
         settingsOverride = true
 
-        tempSettings.flagWindowX = ui.slider("Flag Left/Right",tempSettings.flagWindowX, 0,1)
-        tempSettings.flagWindowY = ui.slider("Flag Up/Down",tempSettings.flagWindowY, 0,1)
+        tempSettings.flagWindowX = ui.slider("Flag Left/Right", tempSettings.flagWindowX, 0, 1)
+        tempSettings.flagWindowY = ui.slider("Flag Up/Down", tempSettings.flagWindowY, 0, 1)
 
 
 
-        if ui.modernButton("Apply Settings",vec2(200,50), ui.ButtonFlags.None, ui.Icons.Save) then
+        if ui.modernButton("Apply Settings", vec2(200, 50), ui.ButtonFlags.None, ui.Icons.Save) then
             betterFlagSettings = tempSettings
             return true
-        else return false
+        else
+            return false
         end
-        
     end,
     function(cancel) --CloseCallback
         settingsOverride = false
-end, ui.OnlineExtraFlags.Tool)
+    end, ui.OnlineExtraFlags.Tool)
 
 
 
 function script.drawUI() --Draws a shitty UI for it.
-
-if settingsOverride then
-    ui.setCursor(vec2(tempSettings.flagWindowX*windowWidth, tempSettings.flagWindowY*windowHeight))
-else
-    ui.setCursor(vec2(betterFlagSettings.flagWindowX*windowWidth, betterFlagSettings.flagWindowY*windowHeight))
-end
-
-flagsWindow:clear()
-flagsWindow:update(function(dt)
-        local blanks = 0
-    for i = 1, #currentFlags do
-
-        if currentFlags[i][1] then
-            ui.drawImage(currentFlags[i][2],vec2((120*(i-blanks)),0),vec2(256+(120*(i-blanks)),256))
-        else
-            blanks = blanks + 1
-        end
+    if settingsOverride then
+        ui.setCursor(vec2(tempSettings.flagWindowX * windowWidth, tempSettings.flagWindowY * windowHeight))
+    else
+        ui.setCursor(vec2(betterFlagSettings.flagWindowX * windowWidth, betterFlagSettings.flagWindowY * windowHeight))
     end
-end)
-ui.image(flagsWindow, vec2(windowWidth,windowHeight))
 
-ui.setCursor(vec2(0,0))
+    flagsWindow:clear()
+    flagsWindow:update(function(dt)
+        local blanks = 0
+        for i = 1, #currentFlags do
+            if currentFlags[i][1] then
+                ui.drawImage(currentFlags[i][2], vec2((120 * (i - blanks)), 0), vec2(256 + (120 * (i - blanks)), 256))
+            else
+                blanks = blanks + 1
+            end
+        end
+    end)
+    ui.image(flagsWindow, vec2(windowWidth, windowHeight))
 
-
-
-
+    ui.setCursor(vec2(0, 0))
 end
