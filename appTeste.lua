3ed340318f381c873c4dc40f28038a3c54c84c68
Rubens da Costa <rubens.costa.tkd@gmail.com>
2026-04-16 00:36:23 -0300

    teste

--- appTeste.lua
+++ appTeste.lua
@@ -3,10 +3,17 @@
 SIM = ac.getSim()
 CAR = ac.getCar(SIM.focusedCar)
 
+SIM.speedLimitKmh = 200
 ac.setDriverChatNameColor(CAR, rgbm(0, 5, 0, 1))
 
-function script.update(dt)
-   
-    
-end
+-- Registrando uma ferramenta extra no chat
+-- Registrando uma ferramenta extra no chat
+ac.registerOnlineExtra('Meu Menu de Admin', function()
+    -- Aqui vai o código da interface (UI)
+    ui.text("Olá, Admin!")
+    ui.button("Dar Kick em alguém")
+        -- Lógica de kick
+end, ui.OnlineExtraFlags.Admin + ui.OnlineExtraFlags.Tool)
+
+
 
