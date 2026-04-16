2690d6ae987240648de97bab42a58ab95eb9c10f
Rubens da Costa <rubens.costa.tkd@gmail.com>
2026-04-16 02:17:37 -0300

    teste de corte

--- appTeste.lua
+++ appTeste.lua
@@ -12,9 +12,13 @@ function script.update(dt)
     if CAR.wheelsOutside > 3 then
            
         ac.debug("Wheels Outside", CAR.wheelsOutside)
-        ac.setDriverChatNameColor(CAR.index, rgbm(5, 0, 0, 1))
+        ac.setDriverChatNameColor(CAR.index, rgbm(5, 0, 0, 1))       
+        ui.drawRaceFlag(ac.FlagType.Ambulance)
+        
+
     elseif CAR.wheelsOutside == 0 then
         ac.setDriverChatNameColor(CAR.index, rgbm(0, 5, 0, 1))
+        nenhuma = ui.drawRaceFlag(ac.FlagType.None)
     end
 end
 
