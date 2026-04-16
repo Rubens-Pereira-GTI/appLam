d7522943e1325ab00b2a6c3f02ac1ee33e254826
Rubens da Costa <rubens.costa.tkd@gmail.com>
2026-04-16 00:57:38 -0300

    teste

--- appTeste.lua
+++ appTeste.lua
@@ -3,19 +3,9 @@
 SIM = ac.getSim()
 CAR = ac.getCar(SIM.focusedCar)
 
-SIM.speedLimitKmh = 200
 ac.setDriverChatNameColor(CAR.index, rgbm(0, 5, 0, 1))
 
-ac.registerOnlineExtra('Meu Menu de Admin', function()
-    ui.text("Olá, Admin!")
 
-    -- O 'if' verifica se o botão foi clicado naquele frame
-    if ui.button("Dar Kick em alguém") then
-        -- Lógica de kick: enviando um comando para o servidor
-        ac.sendChatMessage("/kick 1") 
-        ac.log("Comando de kick enviado!")
-    end
-end, ui.OnlineExtraFlags.Admin + ui.OnlineExtraFlags.Tool)
 
 function script.update(dt)
    
