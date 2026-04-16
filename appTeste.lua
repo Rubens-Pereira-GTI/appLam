50ceec62ce600503f8693ac8a4f027460a47feed
Rubens da Costa <rubens.costa.tkd@gmail.com>
2026-04-16 00:55:08 -0300

    teste

--- appTeste.lua
+++ appTeste.lua
@@ -4,7 +4,7 @@ SIM = ac.getSim()
 CAR = ac.getCar(SIM.focusedCar)
 
 SIM.speedLimitKmh = 200
-ac.setDriverChatNameColor(CAR, rgbm(0, 5, 0, 1))
+ac.setDriverChatNameColor(CAR.index, rgbm(0, 5, 0, 1))
 
 ac.registerOnlineExtra('Meu Menu de Admin', function()
     ui.text("Olá, Admin!")
