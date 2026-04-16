fdfddb5b4ed20724bcfad05dd47d31b3cba70639
Rubens da Costa <rubens.costa.tkd@gmail.com>
2026-04-16 00:08:48 -0300

    teste

--- appTeste.lua
+++ appTeste.lua
@@ -3,7 +3,7 @@
 SIM = ac.getSim()
 CAR = ac.getCar(SIM.focusedCar)
 
-ac.setDriverChatNameColor(CAR, rgbm(2, 0, 0, 1))
+ac.setDriverChatNameColor(CAR, rgbm(0, 5, 0, 1))
 
 function script.update(dt)
    
