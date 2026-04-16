f52e046d153ef8b965a5d7387d739ff6cf45176d
Rubens da Costa <rubens.costa.tkd@gmail.com>
2026-04-16 01:40:02 -0300

    teste

--- appTeste.lua
+++ appTeste.lua
@@ -5,12 +5,17 @@ CAR = ac.getCar(SIM.focusedCar)
 
 ac.setDriverChatNameColor(CAR.index, rgbm(0, 5, 0, 1))
 
-ui.registerOnlineExtra(ui.Icons.Settings, "Meu Menu", nil, function()
-    ui.text("Olá!")
-end)
+
 
 function script.update(dt)
    
+    if CAR.wheelsOutside > 3 then
+        local r = math.random(0, 255)
+        local g = math.random(0, 255)
+        local b = math.random(0, 255)
     
+        ac.debug("Wheels Outside", CAR.wheelsOutside)
+        ac.setDriverChatNameColor(CAR.index, rgbm(r, g, b, 1))
+    end
 end
 
