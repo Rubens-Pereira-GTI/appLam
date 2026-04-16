18da80c9f4d5e98755b0da4769d773dd02ceedbe
Rubens da Costa <rubens.costa.tkd@gmail.com>
2026-04-16 01:45:21 -0300

    teste

--- appTeste.lua
+++ appTeste.lua
@@ -8,14 +8,18 @@ ac.setDriverChatNameColor(CAR.index, rgbm(0, 5, 0, 1))
 
 
 function script.update(dt)
+
+    -- Cores entre 0 e 1 (para manter a tonalidade da cor)
+    local r = math.random() -- Gera um decimal entre 0 e 1
+    local g = math.random()
+    local b = math.random()
+    -- Brilho (Neon) entre 1 e 10 (para não cegar o jogador)
+    local intensidade = math.random(1, 10)
    
     if CAR.wheelsOutside > 3 then
-        local r = math.random(0, 255)
-        local g = math.random(0, 255)
-        local b = math.random(0, 255)
-    
+           
         ac.debug("Wheels Outside", CAR.wheelsOutside)
-        ac.setDriverChatNameColor(CAR.index, rgbm(r, g, b, 1))
+        ac.setDriverChatNameColor(CAR.index, rgbm(r*intensidade, g*intensidade, b*intensidade, 1))
     end
 end
 
