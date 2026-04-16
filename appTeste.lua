a561f5f3c656ce4494fe405ec5e3a34a26764d1a
Rubens da Costa <rubens.costa.tkd@gmail.com>
2026-04-16 01:23:52 -0300

    teste

--- appTeste.lua
+++ appTeste.lua
@@ -5,12 +5,9 @@ CAR = ac.getCar(SIM.focusedCar)
 
 ac.setDriverChatNameColor(CAR.index, rgbm(0, 5, 0, 1))
 
-ac.registerOnlineExtra('Meu Menu de Admin', function()
-    ui.text("Olá, Admin!")
-    if ui.button("Dar Kick em alguém") then
-        ac.sendChatMessage("/kick 1") 
-    end
-end, ui.OnlineExtraFlags.Admin + ui.OnlineExtraFlags.Tool)
+ui.registerOnlineExtra(ui.Icons.Settings, "Meu Menu", nil, function()
+    ui.text("Olá!")
+end)
 
 function script.update(dt)
    
